27e1ab29ff6410646db1d25ae7305e42f07dda1f
TeddyWestside <carstenschober93@googlemail.com>
2017-05-03 20:15:34 +0200

    Bootstrap init
    initialize the bootstrap environment

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="de">
+<html>
+	<head>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<script src="js/jquery.min.js"></script>
+	</head>
+	<body>
+ 
+	  <div class="jumbotron text-center">
+	  <h1>Kitaprognose</h1>
+	  <p>Einfach eine klasse Seite!</p>
+	</div>
+
+	<div class="container">
+	  <div class="row">
+		<div class="col-sm-4">
+		  <h3>Column 1</h3>
+		  <p>Lorem ipsum dolor..</p>
+		  <p>Ut enim ad..</p>
+		</div>
+		<div class="col-sm-4">
+		  <h3>Column 2</h3>
+		  <p>Lorem ipsum dolor..</p>
+		  <p>Ut enim ad..</p>
+		</div>
+		<div class="col-sm-4">
+		  <h3>Column 3</h3>
+		  <p>Lorem ipsum dolor..</p>
+		  <p>Ut enim ad..</p>
+		</div>
+	  </div>
+	</div>
+	
+	
+ 
+ 
+ 
+ 
+ </body>
+ </html>
+ 
