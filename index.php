4452f052d0e3427cc48b320c17baeea4dcb1e02f
JKusber <johannes@kusber.eu>
2017-05-10 14:44:57 +0200

    Bootstrap changed to materialcss
    Bootstrap entfernt und materialCSS Daten hinzugefügt

--- index.php
+++ index.php
@@ -1,42 +1,1 @@
-<!DOCTYPE html>
-<html lang="de">
-<html>
-	<head>
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<script src="js/jquery.min.js"></script>
-	</head>
-	<body>
- 
-	  <div class="jumbotron text-center">
-	  <h1>Kitaprognose</h1>
-	  <p>Einfach eine klasse Seite!</p>
-	</div>
-
-	<div class="container">
-	  <div class="row">
-		<div class="col-sm-4">
-		  <h3>Column 1</h3>
-		  <p>Lorem ipsum dolor..</p>
-		  <p>Ut enim ad..</p>
-		</div>
-		<div class="col-sm-4">
-		  <h3>Column 2</h3>
-		  <p>Lorem ipsum dolor..</p>
-		  <p>Ut enim ad..</p>
-		</div>
-		<div class="col-sm-4">
-		  <h3>Column 3</h3>
-		  <p>Lorem ipsum dolor..</p>
-		  <p>Ut enim ad..</p>
-		</div>
-	  </div>
-	</div>
-	
-	
- 
- 
- 
- 
- </body>
- </html>
- 
+<!DOCTYPE html>  <html>    <head>      <!--Import Google Icon Font-->      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">      <!--Import materialize.css-->      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>      <!--Let browser know website is optimized for mobile-->      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>    </head>    <body>      <!--Import jQuery before materialize.js-->      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>      <script type="text/javascript" src="js/materialize.min.js"></script>	     </body>  </html>
