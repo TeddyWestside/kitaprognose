6f409337d3fd8948d4a9ac9156e4e2c48959571d
TeddyWestside <carstenschober93@googlemail.com>
2017-05-11 21:54:55 +0200

    Added Header and Footer
    Header und Footer hinzugefügt.

---
+++ header.php
@@ -0,0 +1,10 @@
+    <nav>
+			<div class="nav-wrapper">
+			  <a href="#" class="brand-logo">Kitaprognose</a>
+			  <ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li><a href="collapsible.html">Sprache</a></li>
+				<li><a href="collapsible.html"><i class="material-icons">language</i></a></li>
+				<li><a class="dropdown-button" href="#!" data-activates="dropdown1">Sprache<i class="material-icons right">arrow_drop_down</i></a></li>
+			  </ul>
+			</div>
+		</nav>
