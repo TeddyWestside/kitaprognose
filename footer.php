b30f9bb2d228e1501b13b2559a81dd5996821991
JKusber <johannes@kusber.eu>
2017-05-28 18:38:02 +0200

    Design Update

--- footer.php
+++ footer.php
@@ -1,23 +1,37 @@
- <footer class="page-footer">
-    <div class="container">
-		<div class="row">
-			<div class="col l6 s12">
-				<h5 class="white-text">Footer Content</h5>
-				<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-			</div>
-			<div class="col l4 offset-l2 s12">
-				<h5 class="white-text">Links</h5>
-				<ul>
-				  <li><a class="grey-text text-lighten-3" href="./Impressum.php">Impressum</a></li>
-				  <li><a class="grey-text text-lighten-3" href="./Datenschutzerklärung.php">Datenschutz</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<div class="footer-copyright">
-		<div class="container">
-		Letzter Datenbestand: 03. Mai 2017
-		<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-		</div>
-	</div>
+<footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col l6">
+        <h5 class="white-text">Kitaprognose</h5>
+        <p class="grey-text text-lighten-4">Ein Projekt von Studierenden des Fachbereichs Informatik an der
+          Westfälischen Hochschule Gelsenkirchen in Kooperation mit der Stadt Gelsenkirchen. Das Projekt entstand
+          im Rahmen einer Ideenfindung zu OpenData.</p>
+        </div>
+        <div class="col l3"
+        <a href="https://www.w-hs.de/"><img class="responsive-img" src="./img/Logo-WHS.gif"></a>
+      </div>
+      <div class="col l3"
+      <a href="https://www.w-hs.de/"><img class="responsive-img" src="./img/Logo-GE.jpg"></a>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="footer-copyright">
+  <div class="container">
+    <div class="row">
+      <div class="col l4">
+        Letzter Datenbestand: 03. Mai 2017
+      </div>
+      <div class="col l4">
+        <a class="grey-text text-lighten-3" href="./Impressum.php">Impressum</a>
+        ||
+        <a class="grey-text text-lighten-3" href="./Datenschutzerklärung.php">Datenschutz</a>
+      </div>
+      <div class="col l4">
+        <a class="grey-text text-lighten-4 right" href="">Made with LOVE in Gelsenkirchen</a>
+      </div>
+    </div>
+  </div>
+</div>
 </footer>
--- header.php
+++ header.php
@@ -1,10 +1,17 @@
-    <nav>
-			<div class="nav-wrapper">
-			  <a href="#" class="brand-logo">Kitaprognose</a>
-			  <ul id="nav-mobile" class="right hide-on-med-and-down">
-				<li><a href="collapsible.html">Sprache</a></li>
-				<li><a href="collapsible.html"><i class="material-icons">language</i></a></li>
-				<li><a class="dropdown-button" href="#!" data-activates="dropdown1">Sprache<i class="material-icons right">arrow_drop_down</i></a></li>
-			  </ul>
-			</div>
-		</nav>
+<nav>
+  <div class="container">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Kitaprognose</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a class='dropdown-button' href='#' data-activates='dropdownLanguage'>Deutsch</a></li>
+        
+        <!-- Dropdown Structure -->
+        <ul id='dropdownLanguage' class='dropdown-content'>
+          <li><a href="#!">Deutsch</a></li>
+          <li><a href="#!">Sprache 2</a></li>
+          <li><a href="#!">Sprache 3</a></li>
+        </ul>
+
+      </div>
+    </div>
+  </nav>
