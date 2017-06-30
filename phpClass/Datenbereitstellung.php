100d871d2ab0c436ed0a59a7f282d4b182c55275
RKanzenbach <rene.kanzenbach@studmail.w-hs.de>
2017-06-30 11:48:00 +0200

    2017-06-30 Datenbereitstellung - Laden aller Datensätze
    Es werden jetzt alle Datensätze der OpenData-Seite geladen und in die
    Datenbank geschrieben

--- phpClass/Datenbereitstellung.php
+++ phpClass/Datenbereitstellung.php
@@ -21,6 +21,12 @@ class Datenbereitstellung {
   const CO_USERNAME   = "root";
   const CO_PASSWORD   = "";
 
+  //Durchschnittliche Anzahl an Kitaplätzen
+  const CO_KITAPLAETZE = 30;
+
+  //Dateipfad zur CSV-Datei mit den manuell gepflegten Kapazitätsplätzen
+  const CO_PFAD_KITAPLAETZE = "../files/Kapazitaeten.csv";
+
   //KLASSENATTRIBUTE
   //----------------------------------------------------------------------------
   //Datenbankverbindung
@@ -82,19 +88,25 @@ class Datenbereitstellung {
   * @author René Kanzenbach
   */
   public function aktualisiere_datenbestand() {
-    //JSON-Result Objekte
-    $lr_kita_result;
-    $lr_stadtteil_result;
+    //Result-Arrays
+    $la_kita_result;
+    $la_stadtteil_result;
 
-    //=>Kitas
-    $lr_kita_result = $this->lade_datensatz(self::CO_OPENDATA_LINK . 'resource_id='
+    //=>Kita-Datenbestand aktuallisieren
+    //--------------------------------------------------------------------------
+    $la_kita_result = $this->lade_datensatz(self::CO_OPENDATA_LINK . 'resource_id='
       . self::CO_RESOURCE_KITA);
-    $this->speicher_kitas($lr_kita_result);
+    $this->speicher_kitas($la_kita_result);
 
-    //=>AlterStadtteil
-    $lr_stadtteil_result = $this->lade_datensatz(self::CO_OPENDATA_LINK
+    //=>AlterStadtteil-Datenbestand aktualisieren
+    //--------------------------------------------------------------------------
+    $la_stadtteil_result = $this->lade_datensatz(self::CO_OPENDATA_LINK
       . 'resource_id=' . self::CO_RESOURCE_STADTTEIL);
-    $this->speicher_stadtteil($lr_stadtteil_result);
+    $this->speicher_stadtteil($la_stadtteil_result);
+
+    //=>Fehlende Kapazitätsplätze manuell eintragen
+    //--------------------------------------------------------------------------
+    // $this->fuelle_leere_kitaplaetze();
   }
 
   /**
@@ -102,7 +114,7 @@ class Datenbereitstellung {
   * Fragt einen neuen Kita-Datensatz über die OpenData-API ab.
   *
   * @param $iv_link: Link zum laden des Datensatzes
-  * @return $lr_obj: Result Datensatz, welcher aus der JSON-Datei des Open-
+  * @return $ra_result: Result Datensatz, welcher aus der JSON-Datei des Open-
   *  data Portals generiert wurde.
   * @author René Kanzenbach
   */
@@ -110,44 +122,67 @@ class Datenbereitstellung {
 
     $lv_json; //JSON-String
     $lr_obj;  //Aus dem JSON-String generiertes Result-Objekt
+    $lv_offset = 0; //Offset-Parameter für OpenData-Portal
+    $lv_dyn_link = ""; //Link mit dynamischen Offset
+    $ra_result = array();       //Return Array mit abgefragten Datensätzen
 
-    //Datensatz als JSON-String anfordern
-    $lv_json = file_get_contents($iv_link);
+    $lv_dyn_link = $iv_link."&offset=";
 
-    //DEBUG
-    // var_dump($lv_json);
+    //Über das OpenData-Portal können pro Aufruf nur max 100 Datensätze abgefragt
+    //werden. Deshalb müssen mehrere Abfragen versendet werden, welche durch
+    //Verwendung des OFFSET-Parameters bei anderen Datensätzen anfangen.
+    do {
 
-    //Anpassen der Attributnamen innerhalb des JSON-Strings, damit diese im nächsten
-    //Schritt erfolgreich in ein Objekt gewandelt werden können
-    $lv_json = str_replace("0_bis_unter_1_jahr__m", "_0bis1m", $lv_json);
-    $lv_json = str_replace("0_bis_unter_1_jahr__w", "_0bis1w", $lv_json);
-    $lv_json = str_replace("1_bis_unter_2_jahre__m", "_1bis2m", $lv_json);
-    $lv_json = str_replace("1_bis_unter_2_jahre__w", "_1bis2w", $lv_json);
-    $lv_json = str_replace("2_bis_unter_3_jahre__m", "_2bis3m", $lv_json);
-    $lv_json = str_replace("2_bis_unter_3_jahre__w", "_2bis3w", $lv_json);
-    $lv_json = str_replace("3_bis_unter_4_jahre__m", "_3bis4m", $lv_json);
-    $lv_json = str_replace("3_bis_unter_4_jahre__w", "_3bis4w", $lv_json);
-    $lv_json = str_replace("4_bis_unter_5_jahre__m", "_4bis5m", $lv_json);
-    $lv_json = str_replace("4_bis_unter_5_jahre__w", "_4bis5w", $lv_json);
-    $lv_json = str_replace("5_bis_unter_6_jahre__m", "_5bis6m", $lv_json);
-    $lv_json = str_replace("5_bis_unter_6_jahre__w", "_5bis6w", $lv_json);
-    $lv_json = str_replace("6_bis_unter_7_jahre__m", "_6bis7m", $lv_json);
-    $lv_json = str_replace("6_bis_unter_7_jahre__w", "_6bis7w", $lv_json);
-    $lv_json = str_replace("7_bis_unter_8_jahre__m", "_7bis8m", $lv_json);
-    $lv_json = str_replace("7_bis_unter_8_jahre__w", "_7bis8w", $lv_json);
-    $lv_json = str_replace("8_bis_unter_9_jahre__m", "_8bis9m", $lv_json);
-    $lv_json = str_replace("8_bis_unter_9_jahre__w", "_8bis9w", $lv_json);
-    $lv_json = str_replace("9_bis_unter_10_jahre__m", "_9bis10m", $lv_json);
-    $lv_json = str_replace("9_bis_unter_10_jahre__w", "_9bis10w", $lv_json);
+      //Link mit dynamischen Offset zusammensetzen
+      $lv_dyn_link = "";
+      $lv_dyn_link = $iv_link."&offset=".$lv_offset;
 
-    //JSON-String in Objekt wandeln
-    $lr_obj = json_decode($lv_json);
+      //Offset erhöhen
+      $lv_offset = $lv_offset + 100;
 
-    //DEBUG
-    var_dump($lr_obj);
+      //Datensatz als JSON-String anfordern
+      $lv_json = file_get_contents($lv_dyn_link);
 
-    //=>Rückgabe der Datensätze
-    return $lr_obj->result;
+      //Anpassen der Attributnamen innerhalb des JSON-Strings, damit diese im nächsten
+      //Schritt erfolgreich in ein Objekt gewandelt werden können
+      $lv_json = str_replace("0_bis_unter_1_jahr__m", "_0bis1m", $lv_json);
+      $lv_json = str_replace("0_bis_unter_1_jahr__w", "_0bis1w", $lv_json);
+      $lv_json = str_replace("1_bis_unter_2_jahre__m", "_1bis2m", $lv_json);
+      $lv_json = str_replace("1_bis_unter_2_jahre__w", "_1bis2w", $lv_json);
+      $lv_json = str_replace("2_bis_unter_3_jahre__m", "_2bis3m", $lv_json);
+      $lv_json = str_replace("2_bis_unter_3_jahre__w", "_2bis3w", $lv_json);
+      $lv_json = str_replace("3_bis_unter_4_jahre__m", "_3bis4m", $lv_json);
+      $lv_json = str_replace("3_bis_unter_4_jahre__w", "_3bis4w", $lv_json);
+      $lv_json = str_replace("4_bis_unter_5_jahre__m", "_4bis5m", $lv_json);
+      $lv_json = str_replace("4_bis_unter_5_jahre__w", "_4bis5w", $lv_json);
+      $lv_json = str_replace("5_bis_unter_6_jahre__m", "_5bis6m", $lv_json);
+      $lv_json = str_replace("5_bis_unter_6_jahre__w", "_5bis6w", $lv_json);
+      $lv_json = str_replace("6_bis_unter_7_jahre__m", "_6bis7m", $lv_json);
+      $lv_json = str_replace("6_bis_unter_7_jahre__w", "_6bis7w", $lv_json);
+      $lv_json = str_replace("7_bis_unter_8_jahre__m", "_7bis8m", $lv_json);
+      $lv_json = str_replace("7_bis_unter_8_jahre__w", "_7bis8w", $lv_json);
+      $lv_json = str_replace("8_bis_unter_9_jahre__m", "_8bis9m", $lv_json);
+      $lv_json = str_replace("8_bis_unter_9_jahre__w", "_8bis9w", $lv_json);
+      $lv_json = str_replace("9_bis_unter_10_jahre__m", "_9bis10m", $lv_json);
+      $lv_json = str_replace("9_bis_unter_10_jahre__w", "_9bis10w", $lv_json);
+
+      //JSON-String in Objekt wandeln
+      $lr_obj = json_decode($lv_json);
+
+      //Neue Datensätze an das Return-Array hängen
+      $ra_result = array_merge($ra_result, $lr_obj->result->records);
+
+      //>>>DEBUG
+      // var_dump($lv_json);
+      // echo "<br>";
+      // var_dump($lr_obj->result);
+      // $lv_i = sizeof($lr_obj->result->records);
+      // echo $lv_i;
+      //<<<DEBUG
+
+    } while (sizeof($lr_obj->result->records) != 0);
+
+    return $ra_result;
   }
 
   /**
@@ -155,14 +190,15 @@ class Datenbereitstellung {
   * Leert die DB-Tabelle "Kitas" und fügt den übergebenen Datenbestand in
   * die Tabelle ein.
   *
-  * @param $ir_kita_result: Result-Objekt der JSON-Kitadatensätze
+  * @param $ia_kita_result: Array mit Kita-Datensatz
   * @author René Kanzenbach
   */
-  private function speicher_kitas($ir_kita_result) {
+  private function speicher_kitas($ia_kita_result) {
 
     /*--DEKLARATION--*/
     $lv_sql_delete;   //SQL-String zum leeren der Kita-Tabelle
     $lr_sql_stmt;     //Prepared SQL Statement
+    $lv_i = 0;
 
     //Parameter für das Prepared SQL Statement
     $id; $name; $art; $traeger; $plz; $ort; $strasse; $bezirk; $stadtteil; $x;
@@ -188,7 +224,7 @@ class Datenbereitstellung {
     $anzahl_der_plaetze, $anzahl_der_gruppen, $betriebsnummer);
 
     //Statement füllen
-    foreach ($ir_kita_result->records as $record) {
+    foreach ($ia_kita_result as $record) {
 
       $id = $record->Id;
       $name = $record->Name;
@@ -212,11 +248,20 @@ class Datenbereitstellung {
       $anzahl_der_gruppen = $record->Anzahl_der_Gruppen;
       $betriebsnummer = $record->Betriebsnummer;
 
+      // //Prüfen, ob für die Kita die Kapazitätsplätze eingefügt sind
+      // if ($record->Anzahl_der_Plaetze == 0) {
+      //   //->Für diese Kita wurden keine Kitaplätze hinterlegt
+      //   //Standardanzahl für Kitaplätze verwenden
+      //   $anzahl_der_plaetze = self::CO_KITAPLAETZE;
+      // }
+
       //Statement ausführen
       if($lr_sql_stmt->execute()) {
+        $lv_i++;
         // echo "Datensatz eingefügt! id=" . $record->Id;
       }
     }
+    echo $lv_i;
   }
 
   /**
@@ -224,14 +269,14 @@ class Datenbereitstellung {
   * Leert die DB-Tabelle 'alterstadtteil' und füllt sie erneut mit dem Inhalt
   * des übergebenen Result-Objektes.
   *
-  * @param $ir_stadtteil_result: Result-Objekt der JSON-AlterStadtteil Datensätze
+  * @param $ia_stadtteil_result: Result-Array der JSON-AlterStadtteil Datensätze
   * @author René Kanzenbach
   */
-  private function speicher_stadtteil($ir_stadtteil_result) {
+  private function speicher_stadtteil($ia_stadtteil_result) {
 
     /*--DEKLARATION--*/
-    $lv_sql_delete;   //SQL-String zum leeren der Kita-Tabelle
-    $lr_sql_stmt;     //Prepared SQL Statement
+    $lv_sql_delete  = "";       //SQL-String zum leeren der Kita-Tabelle
+    $lr_sql_stmt    = null;     //Prepared SQL Statement
 
     //Statement-Parameter
     $stichtag; $bezirk_id; $bezirk_bez; $stadtteil_id; $stadtteil_bez; $_0bis1m;
@@ -261,7 +306,7 @@ class Datenbereitstellung {
     $_8bis9w, $_9bis10m,$_9bis10w, $gesamtstadt);
 
     //Daten einfügen
-    foreach ($ir_stadtteil_result->records as $record) {
+    foreach ($ia_stadtteil_result as $record) {
 
       //DEBUG
       // var_dump($record);
@@ -301,14 +346,14 @@ class Datenbereitstellung {
   }
 
   /**
-   * FORMATIERE_DATUM
-   * Bringt das Datum vom OpenData-Format "TT.MM.JJJJ" in das SQL-Format
-   * "JJJJ-MM-TT".
-   *
-   * @param $iv_datum: Datum im Format "TT.MM.JJJJ"
-   * @return $rv_datum: Datum im Format "JJJJ-MM-TT"
-   * @author René Kanzenbach
-   */
+  * FORMATIERE_DATUM
+  * Bringt das Datum vom OpenData-Format "TT.MM.JJJJ" in das SQL-Format
+  * "JJJJ-MM-TT".
+  *
+  * @param $iv_datum: Datum im Format "TT.MM.JJJJ"
+  * @return $rv_datum: Datum im Format "JJJJ-MM-TT"
+  * @author René Kanzenbach
+  */
   private function formatiere_datum($iv_datum) {
 
     $rv_datum;
@@ -318,6 +363,68 @@ class Datenbereitstellung {
     $rv_datum = $lv_jahr . '-' . $lv_monat . '-' . $lv_tag;
     return $rv_datum;
   }
- }
 
+
+  /**
+  * FUELLE_LEERE_KITAPLAETZE
+  * Prüft ob Kindertagesstädten existieren, für die keine Kapazitätsplätze
+  * gepflegt sind und füllt diese wenn möglich mit selbst ermittelten Daten.
+  *
+  * @author René Kanzenbach
+  */
+  private function fuelle_leere_kitaplaetze() {
+
+    $lv_sql = "";       //SQL-Statement
+    $lr_kitas_leer  ;   //Kitas, für die keine Kapazitäten gepflegt sind
+    $lr_datei = null;   //Ressource der Kapazitaeten.csv
+    $la_manuell_kap ;   //Array der manuell gepflegten Kapazitäten
+
+    //=>Auslesen aller Kitas, für die keine Kapazitätsplätze gepflegt wurden
+    //--------------------------------------------------------------------------
+    $lv_sql = "SELECT * FROM kitaprognose.kitas";
+
+    //Verbindung prüfen
+    if ($this->gr_conn->connect_error) {
+      //->Es besteht keine Datenbankverbindung
+      die("Es konnte nicht auf die Datenbank zugegriffen werden!");
+    }
+
+    //Statement ausführen
+    $lr_kitas_leer = $this->gr_conn->query($lv_sql);
+
+    //>>>DEBUG
+    // echo "la_kitas_leer: ";
+    // if ($lr_kitas_leer->num_rows > 0) {
+    //   echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    //   // output data of each row
+    //   while($row = $lr_kitas_leer->fetch_assoc()) {
+    //     echo "<tr><td>".$row["Id"]."</td><td>".$row["Name"]."</td></tr>";
+    //   }
+    //   echo "</table>";
+    // }
+    // echo "<br>";
+    //<<<DEBUG
+
+    //=>Auslesen der manuellen gepflegten Kapazitätsplätze
+    //--------------------------------------------------------------------------
+    $lr_datei = fopen(self::CO_PFAD_KITAPLAETZE, "r")
+      or die("Datei mit den manuellen Kapaziätsplätzen konnte nicht geöffnet werden");
+
+    //CSV-Datei in Array konvertieren
+    $la_manuell_kap = fgetcsv($lr_datei);
+
+    //>>>DEBUG
+    // echo "la_manuell_kap: ";
+    // var_dump($la_manuell_kap);
+    // echo "<br>";
+    //<<<DEBUG
+
+    //=>Prüfen ob für die Kitas manuelle Daten vorliegen
+    //--------------------------------------------------------------------------
+
+    //=>Update der Kapazitätsplätze
+    //--------------------------------------------------------------------------
+  }
+
+}
 ?>
