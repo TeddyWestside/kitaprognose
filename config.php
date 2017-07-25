0a43bf48e6b34dfb9571aca243da66c64529c7c8
RKanzenbach <rene.kanzenbach@studmail.w-hs.de>
2017-07-25 17:14:14 +0200

    #4

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'lang' => 'de',
+    'langCode' => 'de',
     'propChildren' => '100',
     'birthrate' => '1.7',
     'host' => 'localhost',
@@ -11,6 +11,5 @@ return array(
     'opendata_link' => 'https://opendata.gelsenkirchen.de/api/action/datastore/search.json?',
     'opendata_link_funk' => 'https://opendata.gelsenkirchen.de/api/3/action/',
     'resource_kita' => '5a27334a-4765-4535-8943-02ef8494f21b',
-    'resource_stadtteil' => 'e5c405d5-3549-433a-ae1c-e81f6c3cc044',
-    'pfad_kitaplaetze' => '../files/Kapazitaeten.csv'
+    'resource_stadtteil' => 'e5c405d5-3549-433a-ae1c-e81f6c3cc044'
 );
